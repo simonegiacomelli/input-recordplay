7a5a796467f1372ad3897eca413c0a754f3a4ea8
simone <simone.giacomelli@gmail.com>
2024-12-07 20:35:10 +0000

    development

--- irp_replay.py
+++ irp_replay.py
@@ -11,8 +11,7 @@ def process_keyboard(line, values: list[str]):
     print(f'Processing line: {line}')
     delay = int(values[2])
     sleep(delay / 1_000_000_000)
-    key = values[1]
-    Key.f11
+    key = getattr(Key, values[1])
     func = keyboard.press if values[0][1] == 'p' else keyboard.release
     func(key)
 
